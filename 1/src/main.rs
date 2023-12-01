e575a7ea543067978cb00598ea8a958113ebbd83
PhilVoel <philipp123.voeller@gmail.com>
2023-12-01 11:35:47 +0100

    Solution to first problem, Part 1

--- 1/src/main.rs
+++ 1/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}", std::fs::read_to_string("input")
+        .expect("Error reading input")
+        .lines()
+        .map(|line|
+             line.chars()
+             .filter(|c| c.is_digit(10))
+             .map(|c| c.to_digit(10).unwrap())
+             .collect::<Vec<u32>>()
+        )
+        .collect::<Vec<Vec<u32>>>()
+        .into_iter()
+        .map(|line| 10*line.first().unwrap()+line.last().unwrap())
+        .sum::<u32>()
+    );
 }
