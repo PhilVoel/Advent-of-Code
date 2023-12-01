12e5e395963f5fc7363ff77df0d8bd0f56a793f1
PhilVoel <philipp123.voeller@gmail.com>
2023-12-01 11:46:21 +0100

    Solution to first problem, Part 2

--- 1/src/main.rs
+++ 1/src/main.rs
@@ -3,7 +3,8 @@ fn main() {
         .expect("Error reading input")
         .lines()
         .map(|line|
-             line.chars()
+             replace_all(line)
+             .chars()
              .filter(|c| c.is_digit(10))
              .map(|c| c.to_digit(10).unwrap())
              .collect::<Vec<u32>>()
@@ -13,4 +14,16 @@ fn main() {
         .map(|line| 10*line.first().unwrap()+line.last().unwrap())
         .sum::<u32>()
     );
+}
+
+fn replace_all(line: &str) -> String {
+    line.replace("one", "o1e")
+        .replace("two", "t2o")
+        .replace("three", "t3e")
+        .replace("four", "f4r")
+        .replace("five", "f5e")
+        .replace("six", "s6x")
+        .replace("seven", "s7n")
+        .replace("eight", "e8t")
+        .replace("nine", "9")
 }
