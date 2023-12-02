22398238837e0fda616e27e6bd743aa6625facef
PhilVoel <philipp123.voeller@gmail.com>
2023-12-02 15:10:41 +0100

    2nd Problem, Part 2

--- 2/src/main.rs
+++ 2/src/main.rs
@@ -1,26 +1,22 @@
 fn main() {
     println!("{}", std::fs::read_to_string("input").unwrap()
              .lines()
-             .filter(|line| line.split_once(':').unwrap()
-                     .1
-                     .split(';')
-                     .all(|set|
-                            set.split(',')
-                            .all(|num_and_color|
-                                match num_and_color.trim().split_once(' ').unwrap() {
-                                    (n, "red") => n.parse::<u8>().unwrap() <= 12,
-                                    (n, "green") => n.parse::<u8>().unwrap() <= 13,
-                                    (n, "blue") => n.parse::<u8>().unwrap() <= 14,
-                                    _ => unreachable!()
-                                }
-                                )
-                          )
-                     )
-             .map(|line| line.split_once(':').unwrap()
-                  .0
-                  .split_once(' ').unwrap()
-                  .1
-                  .parse::<u16>().unwrap())
-             .sum::<u16>()
-             );
+             .map(|line| {
+                 let mut red = Vec::new();
+                 let mut blue = Vec::new();
+                 let mut green = Vec::new();
+                 for set in line.split_once(':').unwrap().1.split(';') {
+                     for num_and_color in set.split(',') {
+                         match num_and_color.trim().split_once(' ').unwrap() {
+                             (n, "red") => red.push(n.parse::<u32>().unwrap()),
+                             (n, "green") => green.push(n.parse::<u32>().unwrap()),
+                             (n, "blue") => blue.push(n.parse::<u32>().unwrap()),
+                             _ => unreachable!()
+                         }
+                     }
+                 }
+                 red.into_iter().max().unwrap() * green.into_iter().max().unwrap() * blue.into_iter().max().unwrap()
+             })
+             .sum::<u32>()
+            );
 }
