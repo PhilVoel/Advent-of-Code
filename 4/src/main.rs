0b8ea1495beb43603efd6f0c8bb93a9617bd1382
PhilVoel <philipp123.voeller@gmail.com>
2023-12-04 12:40:25 +0100

    Day 4, Part 2

--- 4/src/main.rs
+++ 4/src/main.rs
@@ -1,31 +1,25 @@
 use std::collections::HashSet;
 
 fn main() {
-    println!("{}", std::fs::read_to_string("input").unwrap()
-             .lines()
-             .map(|line| {
-                 let (all_win, all_have) = line.split_once(':').unwrap()
-                     .1
-                     .split_once('|').unwrap();
-                 let mut wins: HashSet<&str> = HashSet::new();
-                 for n in all_win.split(' ') {
-                     if n == "" {
-                         continue;
-                     }
-                     wins.insert(n);
-                 }
-                 let mut score = 0;
-                 for n in all_have.split(' ') {
-                     if wins.contains(n) {
-                         if score == 0 {
-                             score = 1;
-                         } else {
-                             score *= 2;
-                         }
-                     }
-                 }
-                 score
-             })
-             .sum::<i32>()
-    );
+    let mut nums = vec![1; 203];
+    for (line_nr, line) in std::fs::read_to_string("input").unwrap()
+        .lines()
+        .enumerate() {
+            let (all_win, all_have) = line.split_once(':').unwrap()
+                .1
+                .split_once('|').unwrap();
+            let mut wins: HashSet<&str> = HashSet::new();
+            for n in all_win.split(' ') {
+                if n == "" {
+                    continue;
+                }
+                wins.insert(n);
+            }
+            for i in (line_nr+1)..=(line_nr+all_have.split(' ')
+                               .filter(|n| wins.contains(n))
+                               .count()) {
+                nums[i] += nums[line_nr];
+            }
+    };
+    println!("{}", nums.iter().sum::<i64>());
 }
