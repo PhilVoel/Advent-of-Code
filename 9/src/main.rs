5c7f253f7affa1f585cacb6bd35b91344f8b003e
PhilVoel <philipp123.voeller@gmail.com>
2023-12-09 15:40:37 +0100

    Day 9, Part 1

---
+++ 9/src/main.rs
@@ -0,0 +1,26 @@
+fn main() {
+    println!("{}", std::fs::read_to_string("input").unwrap()
+             .lines()
+             .map(|line| line.split(" ")
+                  .filter(|s| !s.is_empty())
+                  .map(|s| s.parse().unwrap())
+                  .collect::<Vec<i64>>()
+                  )
+             .map(get_next)
+             .sum::<i64>()
+    );
+}
+
+fn get_next(nums: Vec<i64>) -> i64 {
+    let mut steps = Vec::new(); 
+    for i in 0..nums.len()-1 {
+        steps.push(nums[i+1] - nums[i]);
+    }
+    nums.last().unwrap() + {
+        if steps.iter().all(|s| *s==0) {
+            0
+        } else {
+            get_next(steps)
+        }
+    }
+}
