0e47fd9133f8055e04b1cc92a510e5a0195fd6ab
PhilVoel <philipp123.voeller@gmail.com>
2023-12-11 12:58:21 +0100

    Day 11, Part 1

---
+++ 11/src/main.rs
@@ -0,0 +1,41 @@
+fn main() {
+    let input: Vec<Vec<char>> = std::fs::read_to_string("input").unwrap()
+        .lines()
+        .map(|l| l.chars()
+             .collect()
+        ).collect();
+    let mut gapsy = Vec::new();
+    for (i, l) in input.iter().enumerate() {
+        if l.iter().all(|c| *c == '.') {
+            gapsy.push(i);
+        }
+    }
+    let mut i = 0;
+    let mut gapsx = Vec::new();
+    while i<input[0].len() {
+        if input.iter().all(|l| l[i] == '.') {
+            gapsx.push(i);
+        }
+        i += 1;
+    }
+    let galaxies: Vec<(i64, i64)> = input.into_iter()
+        .enumerate()
+        .map(|(y, l)| l.into_iter()
+             .enumerate()
+             .filter_map(|(x, c)| if c == '#' {
+                 let xoffset = gapsx.iter().filter(|r| x > **r).count() as i64;
+                 let yoffset = gapsy.iter().filter(|r| y > **r).count() as i64;
+                 Some((x as i64 + xoffset, y as i64 + yoffset))
+             } else {
+                 None
+             }).collect::<Vec<_>>()
+        ).flatten()
+        .collect();
+    let mut accum = 0;
+    for (i, galaxy1) in galaxies.iter().enumerate() {
+        for galaxy2 in galaxies.iter().skip(i+1) {
+            accum+= (galaxy1.0-galaxy2.0).abs() + (galaxy1.1-galaxy2.1).abs();
+        } 
+    }
+    println!("{accum}");
+}
