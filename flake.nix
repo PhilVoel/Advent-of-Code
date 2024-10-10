262c1d31977309b6a54a172022fb5f451634d331
PhilVoel <philipp123.voeller@gmail.com>
2024-10-10 04:42:46 +0200

    flake.nix

--- flake.nix
+++ flake.nix
@@ -1,22 +1,26 @@
 {
 	description = "Advent of Code 2023 Ocaml";
-	inputs.nixpkgs.url = "github:nixos/nixpkgs/24.05";
 
-	outputs = {nixpkgs, ...}:
-	let
-		system = "x86_64-linux";
-		pkgs = nixpkgs.legacyPackages."${system}".pkgs;
-	in {
-		devShells."${system}".default = pkgs.mkShell {
-			name = "Ocaml Shell";
-			packages = with pkgs; [
-				dune_3
-				ocaml
-			] ++ (with ocamlPackages;
-			[
-				findlib
-				ocamlformat
-			]);
-		};
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs/24.05";
+		flake-utils.url = "github:numtide/flake-utils/v1.0.0";
 	};
+
+	outputs = {flake-utils, nixpkgs, ...}:
+		flake-utils.lib.eachDefaultSystem (system:
+			let pkgs = nixpkgs.legacyPackages."${system}";
+			in {
+				devShells.default = pkgs.mkShell {
+					name = "Ocaml Shell";
+					packages = with pkgs; [
+						dune_3
+						ocaml
+					] ++ (with ocamlPackages;
+					[
+						findlib
+						ocamlformat
+					]);
+				};
+			}
+		);
 }
